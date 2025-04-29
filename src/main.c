cd62e5b345b43e45a19548d4bfc574d4b3fbb339
tiagoavrodrigues <tiagoavrodrigues@gmail.com>
2025-04-29 15:56:20 +0100

    added unitlist initialization and listing

--- src/main.c
+++ src/main.c
@@ -9,6 +9,8 @@ int main(int argc, char *argv[]){
 
     clearScreen();
     AREALIST areaList = createAreaList();
+    UNITLIST unitList = createUnitList();
     listAreas(&areaList);
+    listUnits(&unitList);
     return 0;
 }
