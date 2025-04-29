bcf17e3d154e15325ca3f6da98f5e255999109e2
tiagoavrodrigues <tiagoavrodrigues@gmail.com>
2025-04-30 00:07:30 +0100

    added HCOURSELIST data strucutre

--- src/include/course.h
+++ src/include/course.h
@@ -4,7 +4,7 @@
 #define COURSE_H
 
 #define DESCRIPTION_MAX_CHAR 101
-#define CODE_MAX_CHAR 5
+#define CODE_MAX_CHAR 9
 
 typedef struct{
     unsigned short id;
@@ -17,13 +17,18 @@ typedef struct{
 } AREALIST;
 
 typedef struct{
-    //enum { CTeSP, Bachelor, Master, Doctorate } type;
+    enum { CTeSP, Bachelor, Master, Doctorate } type;
     char code[CODE_MAX_CHAR];
     char description[DESCRIPTION_MAX_CHAR];
     AREA area;
     UNIT school;
-    enum { Open, Closed } status;
+    enum { Closed, Open } status;
 } HCOURSE; //Higher Course
+
+typedef struct hcourselist{
+    unsigned short count;
+    HCOURSE *items;
+}HCOURSELIST;
 
 typedef struct{
     char code[CODE_MAX_CHAR];
@@ -32,6 +37,9 @@ typedef struct{
 } COURSE;
 
 AREALIST createAreaList();
-void listAreas();
+AREA* getArea(AREA*, unsigned short id);
+void listAreas(AREALIST*);
+HCOURSELIST createHCourseList(AREA*, UNIT*);
+void listHCourses(HCOURSELIST*);
 
 #endif
