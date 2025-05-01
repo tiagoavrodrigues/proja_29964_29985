a976f99a9a3770e09e72ad97383e363ab5398975
tiagoavrodrigues <tiagoavrodrigues@gmail.com>
2025-05-01 21:52:16 +0100

    added USERLIST struct

--- src/include/user.h
+++ src/include/user.h
@@ -1,10 +1,23 @@
 #ifndef USER_H
 #define USER_H
 
+#define USERNAME_MAX_CHAR 21
+#define PASSWORD_MAX_CHAR 21
+
+#define USER_FILENAME "include/user.dat"
+
 typedef struct user{
     enum { Admin, Applicant } type;
-    unsigned char username[21];
-    unsigned char password[21];
+    unsigned char username[USERNAME_MAX_CHAR];
+    unsigned char password[PASSWORD_MAX_CHAR];
 } USER;
+
+typedef struct userlist{
+    unsigned short count;
+    USER *users;
+} USERLIST;
+
+USERLIST createUserList();
+short registerNewUser(USERLIST *userList, USER newUser);
 
 #endif
