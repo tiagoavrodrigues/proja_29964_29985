a9e488639816724f520cb5437e01c36eaa4af63b
tiagoavrodrigues <tiagoavrodrigues@gmail.com>
2025-05-01 10:36:07 +0100

    added enableANSI() fucntion for color codes on windows

--- src/include/utils.h
+++ src/include/utils.h
@@ -1,6 +1,9 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+#ifdef _WIN32
+void enableANSI(); // ANSI CODES PARA TEXTO COLORIDO NA LINHA DE COMANDOS DO WINDOWS
+#endif
 void clearScreen();
 void clearBuffer();
 void pause();
