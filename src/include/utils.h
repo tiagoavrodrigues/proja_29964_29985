42a7aad8b54f38cd68f081ebfa7ec1cb00529400
tiagoavrodrigues <tiagoavrodrigues@gmail.com>
2025-05-01 13:39:37 +0100

    added color enum and printColored() function prototype header

--- src/include/utils.h
+++ src/include/utils.h
@@ -4,9 +4,23 @@
 #ifdef _WIN32
 void enableANSI(); // ANSI CODES PARA TEXTO COLORIDO NA LINHA DE COMANDOS DO WINDOWS
 #endif
+
+#define RED   "\033[1;31m\0"
+#define GREEN "\033[1;32m\0"
+#define BLUE  "\033[1;34m\0"
+#define YELLOW "\033[1;33m\0"
+#define RESET "\033[0m\0"
+
+typedef enum eColor{
+    Red,
+    Green,
+    Yellow,
+    Blue
+}eColor;
+
 void clearScreen();
 void clearBuffer();
 void pause();
 void printFixedWidth(char string[], char fillerChar, unsigned short width);
-
+void printColored(char string[], eColor color, unsigned short width);
 #endif
