930f4b1d18b240a2672b8a621fbeef388d16aac8
tiagoavrodrigues <tiagoavrodrigues@gmail.com>
2025-05-01 23:47:17 +0100

    added getApplicant() function, addApplicant(), editApplicantInfo(), saveApplicantData(), loadApplicantData()

--- src/applicant.c
+++ src/applicant.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "include/applicant.h"
+#include "include/user.h"
 
 int isValidDate(unsigned short day, unsigned short month, unsigned short year){
 
@@ -12,4 +14,74 @@ int isValidDate(unsigned short day, unsigned short month, unsigned short year){
     }
 
     return 1;
+}
+
+APPLICANT* getApplicant(APPLICANTLIST applicantList, USER loggedUser){
+    if(applicantList.count == 0) return NULL;
+
+    APPLICANT applicants[] = applicantList.applicants;
+
+    for(unsigned int i = 0; i < applicantList.count; i++){
+        if(strcmp(applicants[i].user.username, loggedUser.username) == 0
+            && strcmp(applicants[i].user.password, loggedUser.password) == 0) return &applicants[i];
+    }
+
+    return NULL;
+}
+
+short addApplicant(APPLICANTLIST *applicantList, APPLICANT newApplicant){
+    if(applicantList == NULL || !applicantList->count || applicantList->applicants == NULL){
+        return -1;
+    }
+
+    APPLICANT *newItems = realloc(applicantList->applicants, (applicantList->count + 1) * sizeof(APPLICANT));
+    if (newItems == NULL) {
+        return -1;
+    }
+
+    applicantList->applicants = newItems;
+    
+    applicantList->applicants[applicantList->count] = newApplicant;
+    applicantList->count++;
+
+    return 0;
+}
+
+short editApplicantInfo(APPLICANT *applicantInfo, APPLICANT newInfo){
+    if(applicantInfo == NULL) return -1;
+
+    *applicantInfo = newInfo;
+
+    return 0;
+}
+
+short saveApplicantData(APPLICANTLIST *applicantList){
+    FILE *fp;
+
+    if((fp = fopen(APPLICANT_FILENAME, "rb")) == NULL){
+        return -1;
+    };
+
+    fwrite(&applicantList->count, sizeof(unsigned short), 1, fp);
+    fwrite(applicantList->applicants, sizeof(APPLICANT), applicantList->count, fp);
+
+    fclose(fp);
+    return 0;
+}
+
+short loadApplicantData(APPLICANTLIST *applicantList){
+    FILE *fp;
+
+    if((fp = fopen(APPLICANT_FILENAME, "rb")) == NULL){
+        return - 1;
+    };
+
+    fread(&applicantList->count, sizeof(unsigned short), 1, fp);
+
+    applicantList->applicants = (APPLICANT *) calloc(applicantList->count, sizeof(APPLICANT));
+
+    fread(applicantList->applicants, sizeof(APPLICANT), applicantList->count, fp);
+
+    fclose(fp);
+    return 0;
 }
