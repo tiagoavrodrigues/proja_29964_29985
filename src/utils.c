ccd414848a964a1c943868cba9903d81600d5553
tiagoavrodrigues <tiagoavrodrigues@gmail.com>
2025-05-01 10:36:27 +0100

    added enableANSI() fucntion for color codes when running on windows

--- src/utils.c
+++ src/utils.c
@@ -4,6 +4,16 @@
 
 #ifdef _WIN32
 #define CLEAR "cls"
+#include <windows.h>
+
+void enableANSI() { // ANSI CODES PARA TEXTO COLORIDO NA LINHA DE COMANDOS DO WINDOWS
+    HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    DWORD dwMode = 0;
+    GetConsoleMode(hOut, &dwMode);
+    dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+    SetConsoleMode(hOut, dwMode);
+}
+
 #else
 #define CLEAR "clear"
 #endif
