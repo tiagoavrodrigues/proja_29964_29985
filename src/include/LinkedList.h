dc123babdf0b5eb7eb7bd79c95eb6a154ddc5bbe
jomicf <osual864@gmail.com>
2025-05-02 23:53:54 +0100

    LinkedList improvments

--- src/include/LinkedList.h
+++ src/include/LinkedList.h
@@ -1,6 +1,8 @@
 #ifndef _LINKEDLIST_H
 #define _LINKEDLIST_H
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 typedef struct LinkedList{
     void*value;
     size_t size;
@@ -9,11 +11,25 @@ typedef struct LinkedList{
 size_t LinkedList_getLength(LinkedList*self){
     size_t count=0;
     LinkedList*aux=self;
-    while (aux!=NULL){
+    while(aux!=NULL){
         count++;
         aux=aux->node;
     }
     return count;
+}
+void LinkedList_append(LinkedList*self,void*value){
+    LinkedList*aux=self;
+    while(aux->node!=NULL){
+        aux=aux->node;
+    }
+    aux->node=(LinkedList*)malloc(sizeof(LinkedList));
+    if(aux->node==NULL){
+        fprintf(stderr,"\nalocação falhou na LinkedList");
+        return;
+    }
+    aux->node->size=self->size;
+    memcpy(aux->node->value,value,self->size);
+    aux->node->node=NULL;
 }
 LinkedList LinkedList_new(size_t size){
     LinkedList result;
