bd2253b20dce572bd3e62a5f6a20fda20c41f32c
tiagoavrodrigues <tiagoavrodrigues@gmail.com>
2025-05-04 01:00:36 +0100

    added new function calls for program flow and moved test code to test() function

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 
 #include "include/utils.h"
 #include "include/course.h"
@@ -7,6 +8,9 @@
 #include "include/user.h"
 #include "include/applicant.h"
 #include "include/interface_utils.h"
+#include "include/menu.h"
+
+void test();
 
 int main(int argc, char *argv[]){
     setlocale(LC_ALL, "");
@@ -15,44 +19,49 @@ int main(int argc, char *argv[]){
     enableANSI();  // ANSI CODES PARA TEXTO COLORIDO NA LINHA DE COMANDOS DO WINDOWS
     #endif
 
+    USERLIST userList = {};
+    if(loadUserData(&userList) < 0) userList = createUserList();
+
+    USER loggedUser = {};
+
     clearScreen();
-    AREALIST areaList = createAreaList();
-    UNITLIST unitList = createUnitList();
-    HCOURSELIST hcourseList = {};
 
-    USERLIST userList = createUserList();
-    APPLICANTLIST applicantList = {};
+    login(userList, &loggedUser);
+
+    clearScreen();
     
+    AREALIST areaList = {};
+    UNITLIST unitList = {};
+    HCOURSELIST hcourseList = {};
+    APPLICANTLIST applicantList = {};
 
+    initMemory(&areaList, &unitList, &hcourseList, &applicantList);
+    
     char* opt[2] = { "A", "B" };
 
     menuSelect(opt, 0, "wW", "sS", 2, "*", "", ' ', CLEAR);
     
+    test(areaList, unitList, &hcourseList, &applicantList);
 
-    if(loadHCourseData(&hcourseList) < 0) hcourseList = createHCourseList(areaList.items, unitList.items);
-
-    if(hcourseList.items == NULL){
-        printf("\nOut of memory!\n");
-        return -1;
-    }
-
-    listAreas(areaList);
-    listUnits(unitList);
-    listHCourses(hcourseList);
-    setHCourseState(getHCourse(hcourseList, "CTESP001"), Closed);
-    addHCourse(&hcourseList, (HCOURSE) { CTeSP, "CTESP000", "Curso de Teste", getArea(areaList.items, 21), getUnit(unitList.items ,0), Closed});
-    editHCourse(getHCourse(hcourseList, "CTESP000"), (HCOURSE) { CTeSP, "CTESP000", "Curso de Teste Alterado", getArea(areaList.items, 21), getUnit(unitList.items ,0), Closed});
-    listHCoursesByUnit(hcourseList, 0);
-    listHCoursesAsc(hcourseList);
-
-    saveHCourseData(&hcourseList);
-
-    printf("\033[1;31mThis text is red.\033[0m\n");
-    printf("\033[1;32mThis text is green.\033[0m\n");
-    printf("\033[1;34mThis text is blue.\033[0m\n");
+    saveMemory(userList, hcourseList, applicantList);
 
     free(userList.users);
     free(hcourseList.items);
 
     return 0;
+}
+
+void test(AREALIST areaList, UNITLIST unitList, HCOURSELIST *hcourseList, APPLICANTLIST *applicantList){
+    listAreas(areaList);
+    listUnits(unitList);
+    listHCourses(*hcourseList);
+    setHCourseState(getHCourse(*hcourseList, "CTESP001"), Closed);
+    addHCourse(hcourseList, (HCOURSE) { CTeSP, "CTESP000", "Curso de Teste", getArea(areaList.items, 21), getUnit(unitList.items ,0), Closed});
+    editHCourse(getHCourse(*hcourseList, "CTESP000"), (HCOURSE) { CTeSP, "CTESP000", "Curso de Teste Alterado", getArea(areaList.items, 21), getUnit(unitList.items ,0), Closed});
+    listHCoursesByUnit(*hcourseList, 0);
+    listHCoursesAsc(*hcourseList);
+
+    printf("\033[1;31mThis text is red.\033[0m\n");
+    printf("\033[1;32mThis text is green.\033[0m\n");
+    printf("\033[1;34mThis text is blue.\033[0m\n");
 }
