56725cedd2a0c22ed833fcf8d3e13e5fe854170b
tiagoavrodrigues <tiagoavrodrigues@gmail.com>
2025-05-04 17:17:25 +0100

    added registerUserInfoForm()

--- src/menu.c
+++ src/menu.c
@@ -3,6 +3,7 @@
 #include "include/menu.h"
 #include "include/utils.h"
 #include "include/user.h"
+#include "include/interface_utils.h"
 
 void drawLoginUsername(){
     printf("\n");
@@ -26,7 +27,7 @@ void drawLoginPassword(char username[]){
     );
 }
 
-short login(USERLIST userList, USER *loggedUser){
+short loginForm(USERLIST userList, USER *loggedUser){
     char username[USERNAME_MAX_CHAR];
     char password[PASSWORD_MAX_CHAR];
 
@@ -36,7 +37,10 @@ short login(USERLIST userList, USER *loggedUser){
     fgets(username, USERNAME_MAX_CHAR, stdin);
     username[strcspn(username, "\n")] = '\0';
     if(!userExists(userList, username)){
-
+        char opt;
+        printf("\n\n > "); printColored("O utilizador não existe! Deseja criar?\n", Yellow, 0);
+        opt = getchar();
+        if(opt == 'S' || opt == 's') registerUserInfoForm(); else return -1;
     }
 
     short result;
@@ -61,4 +65,80 @@ short login(USERLIST userList, USER *loggedUser){
     }while(result < 1);
 
     return 0;
+}
+
+void registerUserInfoForm(){
+    APPLICANT newApplicant = {};
+    
+    unsigned short validInput = 0;
+
+    while(validInput == 0){
+        validInput = 1;
+        clearScreen();
+        printf(
+            "+------------------------------------------------+\n"
+            "|     FORMULÁRIO DE REGISTO - PARTE [1 / 2]      |\n"
+            "+------------------------------------------------+\n"
+        );
+        printf("| Primeiro nome: %s%c", newApplicant.name[0] == '\0' ? "" : newApplicant.name, newApplicant.name[0] == '\0' ? "" : "\n" );
+        if(newApplicant.name[0] == '\0'){
+            clearBuffer();
+            fgets(newApplicant.name, NAME_MAX_CHAR, stdin);
+            newApplicant.name[strcspn(newApplicant.name, "\n")] = '\0';
+        };
+        printf("| Último nome: %s%c", newApplicant.surname[0] == '\0' ? "" : newApplicant.surname, newApplicant.surname[0] == '\0' ? "" : "\n" );
+        if(newApplicant.surname[0] == '\0'){
+            fgets(newApplicant.surname, NAME_MAX_CHAR, stdin);
+            newApplicant.surname[strcspn(newApplicant.surname, "\n")] = '\0';
+        };
+        printf("| Número de CC: %s%c", newApplicant.nCC[0] == '\0' ? "" : newApplicant.nCC, newApplicant.nCC[0] == '\0' ? "" : "\n" );
+        if(newApplicant.nCC[0] == '\0'){
+            clearBuffer();
+            inputNumber(newApplicant.nCC, NCC_MAX_CHAR);
+            if(strlen(newApplicant.nCC) < NCC_MAX_CHAR){
+                printf("\n\n > "); printColored("O número de CC não é válido!\n", Red, 0); _pause();
+                newApplicant.nCC[0] = '\0';
+                validInput = 0;
+            }
+        }
+        if(validInput == 1){
+            printf("| NIF:  %s", newApplicant.nif[0] == '\0' ? "" : newApplicant.nif);
+            if(newApplicant.nif[0] == '\0'){
+                inputNumber(newApplicant.nif, NIF_MAX_CHAR);
+                if(strlen(newApplicant.nif) < NIF_MAX_CHAR){
+                    printf("\n\n > "); printColored("O NIF não é válido!\n", Red, 0); _pause();
+                    newApplicant.nif[0] = '\0';
+                    validInput = 0;
+                }
+            }
+        }
+        if(validInput == 1){
+            printf("| Data de nascimento "); printColored("(dd/mm/aaaa): ", Yellow, 0);
+            scanf("%2d/%2d/%4d", &newApplicant.dateofbirth.day, &newApplicant.dateofbirth.month, &newApplicant.dateofbirth.year);
+            validInput = isValidDate(newApplicant.dateofbirth.day, newApplicant.dateofbirth.month, newApplicant.dateofbirth.year);
+            if(validInput == 0){
+                printf("\n\n > "); printColored("A data não é válida!\n", Red, 0); _pause();
+            }
+        }
+    }
+    while(validInput == 0){
+        validInput = 1;
+        clearScreen();
+        printf(
+            "+------------------------------------------------+\n",
+            "|     FORMULÁRIO DE REGISTO - PARTE [2 / 2]      |\n",
+            "+------------------------------------------------+\n"
+        );
+        printf("\n| Escola frequentada: %s", newApplicant.school[0] == '\0' ? "" : newApplicant.school[0]);
+        fgets(newApplicant.school, SCHOOL_MAX_CHAR, stdin);
+        newApplicant.school[strcspn(newApplicant.school, "\n")] = '\0';
+        printf("\n| Média de fim de ano: ");
+        scanf(" %f", &newApplicant.mean);
+        if(newApplicant.mean < 0 && newApplicant.mean > 20){
+            printf("\n\n > "); printColored("Valor inválido! O valor deverá ser entre 0 e 20.", Red, 0); _pause();
+            validInput = 0;
+        }
+
+
+    }
 }
